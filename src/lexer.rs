cf877ca1611c007f73190a290d272343863d87ec
Irwin <rodriguez.irwin@gmail.com>
2021-12-22 22:55:31 +0100

    lexer and token

---
+++ src/lexer.rs
@@ -0,0 +1,162 @@
+use crate::token::*;
+
+use std::iter::Iterator;
+use std::iter::Peekable;
+use std::str::Chars;
+
+pub struct Lexer<'a> {
+    input: Peekable<Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &str) -> Lexer {
+        Lexer {
+            input: input.chars().peekable(),
+        }
+    }
+
+    pub fn read_char(&mut self) -> Option<char> {
+        self.input.next()
+    }
+
+    pub fn peek_char(&mut self) -> Option<&char> {
+        self.input.peek()
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        match self.read_char() {
+            Some('=') => {
+                let eq = match self.peek_char() {
+                    Some(&ch) => ch == '=',
+                    None => false,
+                };
+                if eq {
+                    self.read_char();
+                    return Token::EQ;
+                } else {
+                    return Token::ASSIGN;
+                }
+            }
+            Some('+') => Token::PLUS,
+            Some('-') => Token::MINUS,
+            Some('!') => {
+                let neq = match self.peek_char() {
+                    Some(&ch) => ch == '=',
+                    None => false,
+                };
+                if neq {
+                    self.read_char();
+                    return Token::NEQ;
+                } else {
+                    return Token::BANG;
+                }
+            }
+            Some('/') => Token::SLASH,
+            Some('*') => Token::ASTERISK,
+            Some('<') => Token::LT,
+            Some('>') => Token::GT,
+            Some(',') => Token::COMMA,
+            Some(':') => Token::COLON,
+            Some('(') => Token::LPAREN,
+            Some(')') => Token::RPAREN,
+            Some('{') => Token::LBRACE,
+            Some('}') => Token::RBRACE,
+            Some('[') => Token::LBRACKET,
+            Some(']') => Token::RBRACKET,
+            Some('"') => self.read_string(),
+            Some(ch) => {
+                if Self::is_letter(ch) {
+                    return self.read_identifier(ch);
+                } else if Self::is_digital(ch) {
+                    return self.read_number(ch);
+                } else {
+                    return Token::ILLEGAL;
+                }
+            }
+            // EOF
+            return None => Token::EOF;
+        }
+    }
+
+    fn peek_is_whitespace(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => ch.is_whitespace(),
+            None => false,
+        }
+    }
+
+    fn peek_is_letter(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => Self::is_letter(ch),
+            None => false,
+        }
+    }
+
+    fn read_identifier(&mut self, ch: char) -> Token {
+        let mut ident = String::new();
+        ident.push(ch);
+        while self.peek_is_letter() {
+            match self.read_char() {
+                Some(ch) => ident.push(ch),
+                None => break,
+            };
+        }
+
+        return Token::lookup_ident(ident);
+    }
+
+    fn read_string(&mut self) -> Token {
+        let mut string = String::new();
+
+        loop {
+            match self.read_char() {
+                Some('"') | None => break,
+                Some(ch) => string.push(ch),
+            };
+        }
+
+        return Token::STRING(string);
+    }
+
+    fn peek_is_number(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => Self::is_digital(ch),
+            None => false,
+        }
+    }
+
+    fn read_number(&mut self, ch: char) -> Token {
+        let mut number = String::new();
+        number.push(ch);
+        while self.peek_is_number() {
+            match self.read_char() {
+                Some(ch) => number.push(ch),
+                None => break,
+            };
+        }
+
+        return Token::INT(number);
+    }
+
+    fn is_letter(ch: char) -> bool {
+        return ch.is_alphabetic() || ch == '_';
+    }
+
+    fn is_digital(ch: char) -> bool {
+        return ch.is_digit(10);
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Self::Item> {
+        let tok = self.next_token();
+        if tok == Token::EOF {
+            None
+        } else {
+            Some(tok)
+        }
+    }
+}
